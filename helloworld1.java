09725efefa541e63f590f66891a9d1ad4927b2ce
mohitchauhan331 <mohitchauhan4793@gmail.com>
2024-03-13 23:28:43 +0530

    code with a class

---
+++ helloworld1.java
@@ -0,0 +1,11 @@
+public class helloworld1 
+{
+    public static void main(String args[])
+    {
+     System.out.println("hi my name is mohit");
+     System.out.println("i am 20 years old");
+     System.out.println("studying in daviet");
+     System.out.println("i live in jalandhar ");   
+    }
+}
+    
