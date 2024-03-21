d37cdfff09148950bb5cfadc7729c182bcb222e5
Mohit Chauhan <159440273+mohitchauhan331@users.noreply.github.com>
2024-03-21 15:42:47 +0530

    Update assignment2.java

--- assignement2.java
+++ assignement2.java
@@ -1,3 +1,6 @@
+/*Write a program that takes your full name as input and displays the abbreviations of the
+first and middle names except the last name which is displayed as it is. 
+For example, if your name is Robert Brett Roser, then the output should be R.B.Roser.*/
 import java.util.Scanner;
 
 public class assignement2
