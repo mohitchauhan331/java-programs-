18aaea60c2f394c5efe90cf9ea9e35d6fa27ab12
Mohit Chauhan <159440273+mohitchauhan331@users.noreply.github.com>
2024-03-21 15:39:53 +0530

    Update assignment.java

--- assignement.java
+++ assignement.java
@@ -1,4 +1,8 @@
-public class assignement {
+ /*Write a Java program to get the character at the given index within the string.
+Sample Output:Original String = Java Exercises!
+The character at position 0 is J
+The character at position 10 is i*/
+    public class assignement {
     public static void main(String argu []) 
     {
         String str = "hello everyone!";
